dd9c1468911045c22e654c1072953ca7a9550e28
alex <a.landar@doctis.ru>
2022-10-07 17:08:02 +0300

    fetch main data from server

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,34 @@
 import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
-  state: {},
+  state: () => ({
+    valuteList: [],
+    lastUpdateDate: null,
+  }),
   getters: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    setValuteList(state, valuteList) {
+      state.valuteList = valuteList;
+    },
+    setLastUpdateDate(state, lastUpdateDate) {
+      state.lastUpdateDate = lastUpdateDate;
+    },
+  },
+  actions: {
+    async fetchValuteList({ commit }) {
+      try {
+        const response = await axios.get(
+          "https://www.cbr-xml-daily.ru/daily_json.js"
+        );
+        console.log(response.data);
+        const data = response.data.
+        commit("setValuteList", response.data.Valute);
+        commit("setLastUpdateDate", response.data.Date);
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  },
   modules: {},
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,64 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h2 class="home" @click="showList">
+    Посмотреть весь список пар валют с переключением основной валюты
+  </h2>
+  <span>Последняя дата обновления: {{ lastUpdateDate }}</span>
+  <div>
+    <div v-for="valute in valuteList" :key="valute.ID">
+      <div>{{ valute.Name }}</div>
+      <div>
+        <div>
+          <div>1 RUB</div>
+          <div>{{ valute.Value }} {{ valute.CharCode }}</div>
+        </div>
+        <div
+          :class="getDifferenceBackgroundColor(valute.Value, valute.Previous)"
+        >
+          {{ getDifference(valute.Value, valute.Previous) }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+// eslint-disable-next-line no-unused-vars
+import { mapState, mapGetters, mapActions, mapMutations } from "vuex";
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      current: "",
+    };
+  },
+  computed: {
+    ...mapState({
+      valuteList: (state) => state.valuteList,
+      lastUpdateDate: (state) => state.lastUpdateDate,
+    }),
+  },
+  methods: {
+    ...mapActions(["fetchValuteList"]),
+    showList() {
+      console.log(this.valuteList);
+    },
+    getDifference(val, prevVal) {
+      return (val - prevVal).toFixed(4);
+    },
+    getDifferenceBackgroundColor(val, prevVal) {
+      return val - prevVal > 0 ? "green" : "red";
+    },
+  },
+  mounted() {
+    this.fetchValuteList();
   },
 };
 </script>
+<style scoped>
+.red {
+  background: red;
+}
+.green {
+  background: green;
+}
+</style>
